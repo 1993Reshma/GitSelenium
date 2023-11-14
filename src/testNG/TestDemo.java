c261057b916aaf54dd9c0960de8f35bf8549748d
1993Reshma <150758980+1993Reshma@users.noreply.github.com>
2023-11-14 08:30:16 +0530

    Updated to firefox

--- src/testNG/TestDemo.java
+++ src/testNG/TestDemo.java
@@ -3,16 +3,18 @@ package testNG;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 
 public class TestDemo {
 	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
+	  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
-	  driver = new ChromeDriver();
+	  driver = new FirefoxDriver();
 	  driver.manage().window().maximize();
   }
 
